ed46c00559ac959f188d7ca35a99a6a7c738b52d
ChapCunningham <chapcunningham@gmail.com>
2025-02-02 20:56:01 -0600

    Update hitter_table_2025.py

--- hitter_table_2025.py
+++ hitter_table_2025.py
@@ -84,11 +84,14 @@ hitters_df = hitters_df.fillna(0)
 # Calculate at-bats (AB)
 bip_counts = df[df['PitchCall'] == 'InPlay'].groupby('Batter').size().reset_index(name='BIP')
 k_counts = df[df['KorBB'] == 'Strikeout'].groupby('Batter').size().reset_index(name='K')
+walk_counts = df[df['KorBB'] == 'Walk'].groupby('Batter').size().reset_index(name='Walks')
 hitters_df = pd.merge(hitters_df, bip_counts, on='Batter', how='left').fillna(0)
 hitters_df = pd.merge(hitters_df, k_counts, on='Batter', how='left').fillna(0)
+hitters_df = pd.merge(hitters_df, walk_counts, on='Batter', how='left').fillna(0)
 hitters_df['AB'] = hitters_df['BIP'] + hitters_df['K'] - hitters_df['Sacrifice']
 
 # Calculate advanced stats
+hitters_df['OBP'] = (hitters_df['Hits'] + hitters_df['Walks'] + hitters_df['HBP']) / hitters_df['PA']
 hitters_df['TotalBases'] = hitters_df['1B'] + (2 * hitters_df['2B']) + (3 * hitters_df['3B']) + (4 * hitters_df['HR'])
 hitters_df['SLG'] = hitters_df['TotalBases'] / hitters_df['AB']
 hitters_df['OPS'] = hitters_df['OBP'] + hitters_df['SLG']
